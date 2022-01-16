93d12ebc694adcfb88d17eef0ed94d405978e4a3
Lorraine <lorraine.marie@live.ca>
2022-01-16 16:08:45 -0500

    new copy of old  code with new to-do list feature in the making :sparkles::heavy_check_mark:

---
+++ src/components/ToDo.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ToDoForm from "./ToDoForm";
+import ToDoList from "./ToDoList";
+// import { IoCloseSharp } from 'react-icons/io';
+// import { AiOutlineEdit } from 'react-icons/ai';
+
+
+function ToDo({toDos, completeToDo}) {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    });
+
+
+
+return toDos.map((toDo, index) => (
+<div className={toDo.isComplete ? 'item complete' : 'item'} key={index}>
+
+<div key={toDo.id} onClick={() => completeToDo(toDo.id)}>
+{toDo.text}
+
+</div>
+<div className='icons'>
+    {/* <IoCloseSharp />
+    <AiOutlineEdit /> */}
+</div>
+</div>
+
+));
+
+}
+
+export default ToDo;
