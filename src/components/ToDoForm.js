364d6db46ac993d0c27c0f2f38152cf25f5283dc
Lorraine <lorraine.marie@live.ca>
2022-01-17 12:08:55 -0500

    deploy test

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,43 +1,43 @@
-import React, {useEffect, useRef, useState} from "react";
+// import React, {useEffect, useRef, useState} from "react";
 
 
-function ToDoForm(props) {
-    const [input, setInput] = useState(props.edit ? props.edit.value : '');
+// function ToDoForm(props) {
+//     const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
-    // const inputRef = useRef(null);
+//     // const inputRef = useRef(null);
 
-    // useEffect(() => {
-    //     inputRef.current.focus('');
-    // });
+//     // useEffect(() => {
+//     //     inputRef.current.focus('');
+//     // });
 
-    const handleChange = dreams => {
-        setInput(dreams.target.value)
-    }
+//     const handleChange = dreams => {
+//         setInput(dreams.target.value)
+//     }
 
-    const handleSubmit = dreams => {
-        dreams.preventDefault();
+//     const handleSubmit = dreams => {
+//         dreams.preventDefault();
 
-        props.onSubmit({
-            id: Math.floor(Math.random() * 10000),
-            text: input
-        });
-        setInput('');
-    };
+//         props.onSubmit({
+//             id: Math.floor(Math.random() * 10000),
+//             text: input
+//         });
+//         setInput('');
+//     };
 
-    return(
-        <form className='to-do-form' onSubmit={handleSubmit}>
-            <input 
-            className='to-do-input'
-            type="text" 
-            placeholder='Add dreams here'
-            value={input}
-            name='text'
-            onChange={handleChange}
-              />
-              <button className='to-do-button'>Add Dream</button>
-        </form>
-    );
-}
+//     return(
+//         <form className='to-do-form' onSubmit={handleSubmit}>
+//             <input 
+//             className='to-do-input'
+//             type="text" 
+//             placeholder='Add dreams here'
+//             value={input}
+//             name='text'
+//             onChange={handleChange}
+//               />
+//               <button className='to-do-button'>Add Dream</button>
+//         </form>
+//     );
+// }
 
 
-export default ToDoForm;
+// export default ToDoForm;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,41 +1,41 @@
-import React, {useState} from "react";
-import ToDo from "./ToDo";
-import ToDoForm from "./ToDoForm";
+// import React, {useState} from "react";
+// import ToDo from "./ToDo";
+// import ToDoForm from "./ToDoForm";
 
-function ToDoList() {
-    const [toDos, setToDos] = useState([]);
+// function ToDoList() {
+//     const [toDos, setToDos] = useState([]);
 
-    const addToDo = toDo => {
-        if(!toDo.text || /^\s$/.test(toDo.text)) {
-            return
-        }
+//     const addToDo = toDo => {
+//         if(!toDo.text || /^\s$/.test(toDo.text)) {
+//             return
+//         }
 
-        const newToDos = [toDo, ...toDos]
+//         const newToDos = [toDo, ...toDos]
 
-        setToDos(newToDos);
+//         setToDos(newToDos);
         
-    };
+//     };
 
-    const completeToDo = id => {
-        let updatedToDos = toDos.map(toDo => {
-            if (toDo.id === id) {
-                toDo.isComplete = !toDo.isComplete
-            }
-            return toDo
-        })
-        setToDos(updatedToDos);
-    }
-
-
-    return (
-        <div>
-            <h3>hopes wishes dreams for you day go here:</h3>
-            <ToDoForm onsubmit={addToDo} />
-            <ToDo 
-            toDos={toDos} completeToDo={completeToDo} />
-        </div>
-    )
-}
+//     const completeToDo = id => {
+//         let updatedToDos = toDos.map(toDo => {
+//             if (toDo.id === id) {
+//                 toDo.isComplete = !toDo.isComplete
+//             }
+//             return toDo
+//         })
+//         setToDos(updatedToDos);
+//     }
 
 
-export default ToDoList
+//     return (
+//         <div>
+//             <h3>hopes wishes dreams for you day go here:</h3>
+//             <ToDoForm onsubmit={addToDo} />
+//             <ToDo 
+//             toDos={toDos} completeToDo={completeToDo} />
+//         </div>
+//     )
+// }
+
+
+// export default ToDoList
